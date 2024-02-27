e4438865e32ea48e61f0d0c41a8b33c50363af76
Daisuke Ueda　上田 大輔 <daisuke.u524@gmail.com>
2024-02-28 07:53:23 +0900

    Add: DynamoDBの読み込みができるロールを作成

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ terraform {
 }
 
 resource "aws_dynamodb_table" "gang_of_straw" {
-  name           = "gang"
+  name           = "gang_of_straw"
   billing_mode   = "PROVISIONED"
   read_capacity  = 20
   write_capacity = 20
@@ -26,6 +26,33 @@ resource "aws_dynamodb_table" "gang_of_straw" {
   }
   attribute {
     name = "bounty"
-    type = "N"
+    type = "S"
   }
 }
+
+resource "aws_iam_role" "dynamodb_read_only" {
+  name = "dynamodb_read_only"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = "sts:AssumeRole"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "cloudwatch_log_writing_permit" {
+  role       = aws_iam_role.dynamodb_read_only.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+resource "aws_iam_role_policy_attachment" "dynamodb_reading_permit" {
+  role       = aws_iam_role.dynamodb_read_only.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonDynamoDBReadOnlyAccess"
+}
+
